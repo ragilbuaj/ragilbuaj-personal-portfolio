07377a85902139665df108db00b84d860027a89e
ragilbuaj <budhiajieragil@gmail.com>
2023-07-29 16:13:26 +0700

    refactor: remove form detection

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,11 +5,7 @@ function Contact() {
 		<section className="mt-3 w-full animate-contentAnimation md:flex md:w-11/12 md:flex-col md:items-center md:justify-center">
 			<hr></hr>
 			<h1 className="mt-6 text-center font-neue-montreal text-3xl font-bold text-orange-200">Get in touch!</h1>
-			<form className="mt-6 grid grid-cols-1 gap-5 md:w-11/12 min-[1440px]:h-full" name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={(e) => e.preventDefault()}>
-				<input type="hidden" name="form-name" value="contact" />
-				<div hidden>
-					<input name="bot-field" />
-				</div>
+			<form className="mt-6 grid grid-cols-1 gap-5 md:w-11/12 min-[1440px]:h-full">
 				<div className="flex flex-col gap-2 min-[1440px]:h-full">
 					<label className="text-md text-orange-200" htmlFor="email">
 						Your email
